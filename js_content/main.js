627151f3026352b1581495550efdaa58d640fb34
jihad <jihad.a@aluno.ufabc.edu.br>
2023-06-09 18:13:03 -0300

    first commit

---
+++ js_content/main.js
@@ -0,0 +1,12 @@
+function tocaLatido() {
+    document.querySelector('#som_latido').play();
+}
+document.querySelector('.tecla_latido').onclick = tocaLatido;
+
+function tocaMiado() {
+    document.querySelector('#som_miado').play();
+}
+document.querySelector('.tecla_miado').onclick = tocaMiado;
+
+
+document.querySelector('.tecla_latido').onclick = tocaLatido;
